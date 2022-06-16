635d372d94fbcf9a639b67535cc1192ba995cbf4
Yotam <yotamsoudry@gmail.com>
2022-06-16 18:25:02 +0300

    creating user database

---
+++ website/models.py
@@ -0,0 +1,17 @@
+from . import db
+import flask_login
+import sqlalchemy
+
+
+class User(db.Model, flask_login.UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    email = db.column(db.String(50), unique=True)
+    birthday = db.Column(db.Date(timezone=True), default=sqlalchemy.sql.func.now())
+    location = db.Column(db.String(50))
+    gender = db.Column(db.String(6))
+
+
