6da574d242ac13c25cafeb3823c8b5baec9bd8b0
Yotam <yotamsoudry@gmail.com>
2022-06-16 15:05:39 +0300

    allowing login pages to accept post requests

--- website/auth.py
+++ website/auth.py
@@ -3,16 +3,16 @@ import flask
 auth = flask.Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return flask.render_template("login.html")
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
     return flask.render_template("register.html")
 
 
-@auth.route('/my-account')
+@auth.route('/my-account', methods=['GET', 'POST'])
 def my_account():
     return flask.render_template("my_account.html")
